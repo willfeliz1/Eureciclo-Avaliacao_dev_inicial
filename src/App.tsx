e8d227746a134f208541de3d72e927606f2e37b4
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-04 16:22:24 -0300

    feat: descending order, accumalate values and if is higher than Gallon then  will cutt rest values

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,109 @@
-import React, { FormEvent, useCallback, useMemo, useState } from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
 
-interface IBottle {
-  quantity: number;
-}
+// interface IBottle {
+//   quantity: number;
+// }
+
+let total = 0;
 
 const App: React.FC = () => {
   const [Gallon, setGallon] = useState(0);
   const [newGallon, setNewGallon] = useState('');
 
-  const [Bottles, setBottles] = useState<IBottle[]>([]);
+  const [Bottles, setBottles] = useState([] as number[]);
   const [newBottle, setNewBottle] = useState('');
 
-  const usedBottles = [];
+  const [usedBottles, setUsedBottles] = useState([] as number[]);
 
+  function compareNumbers(a: number, b: number) {
+    return b - a;
+  }
 
   const handleAddBottle = useCallback(async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
 
-    Bottles?.push({
-      quantity: Number(newBottle),
-    })
+    Bottles.push(Number(newBottle));
 
     setBottles([...Bottles]);
-
   }, [Bottles, newBottle]);
 
   const handleAddGallon = useCallback(async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
 
+    setGallon(Number(newGallon));
 
-
-    setGallon(Number(newGallon));   
   }, [newGallon]);
 
-  const getHigherValueBottles = useCallback((arr: Array<IBottle>): Number => {
-    const arrayValues = arr.map(state => state.quantity);
-
-    return Math.max(...arrayValues);
-  }, []);
 
 
-  const bottlesUsed = useMemo(() => {
+  const Result = useCallback(() => {
+    // let bottlesValueArray = Bottles.map(state => state);
 
+    let ordenedBottles = Bottles.map(state => state);
 
-    console.log(getHigherValueBottles(Bottles));
-  }, [Bottles, getHigherValueBottles])
+    ordenedBottles.sort(compareNumbers);
+
+    ordenedBottles.reduce((acc, currentValue, currentIndex, array) => {
+      total = acc + currentValue;
+
+      if (total > Gallon || currentValue > Gallon) {
+        // array.splice(currentIndex);
+
+        ordenedBottles.splice(currentIndex);
+
+        setUsedBottles([...ordenedBottles]);
+
+      }
+
+      return acc + currentValue;
+
+    }, 0);
+
+  }, [Bottles, Gallon])
 
 
   return (
     <>
-      <header style={{marginTop: '25px'}}>
+      <header style={{ marginTop: '25px' }}>
         <strong>Insira o volume do galão</strong>
-        <form onSubmit={handleAddGallon} style={{ flexDirection: "column", marginTop: "5px"}} >
-          <input 
-            placeholder="Quantidade de litros" 
+        <form onSubmit={handleAddGallon} style={{ flexDirection: "column", marginTop: "5px" }} >
+          <input
+            placeholder="Quantidade de litros"
             type="number"
             onChange={e => setNewGallon(e.target.value)}
           />
           <button type="submit">Inserir</button>
         </form>
       </header>
-   
-      <div style={{marginTop: '15px'}}>
+
+      <div style={{ marginTop: '15px' }}>
         <strong>Insira volume da garrafa: </strong>
 
-        <form onSubmit={handleAddBottle} style={{ flexDirection: "column", marginTop: "5px"}} >
-          <input 
+        <form onSubmit={handleAddBottle} style={{ flexDirection: "column", marginTop: "5px" }} >
+          <input
             placeholder="Quantidade de litros"
-            type="number"
             onChange={e => setNewBottle(e.target.value)}
           />
           <button type="submit">Inserir</button>
         </form>
-        
       </div>
 
-      <div style={{marginTop: '50px'}}>
-        <h4>RESULTADO</h4>
-        <div style={{display: "flex", flexDirection: "column"}}>
+      <div style={{ marginTop: '30px' }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <strong style={{ marginBottom: '20px' }}>Galão: {Gallon}L</strong>
+
           {Bottles.map((bottle, index) => (
             <div key={index}>
-              <span>Garrafa {index}: {bottle.quantity} </span>
-
-              
+              <span>Garrafa {index}: {bottle} </span>
             </div>
           ))}
-          <strong style={{marginTop: '20px'}}>Galão: {newGallon}L</strong>
+
+          <strong style={{ marginTop: '10px' }}>Garrafas usadas: {usedBottles}</strong>
           
-          <div>
-          <span>Garrafas usadas: {bottlesUsed}</span>          
-          </div>
+          <button type="button" onClick={Result} style={{ maxWidth: '200px', marginTop: '20px' }}>RESULTADO</button>
         </div>
       </div>
     </>
-      
-
   );
 }
 
